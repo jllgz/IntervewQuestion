6f37c3ce0dd877d9f9db0a38b0fccb9a1a3a3575
jllgz <josephlobligar@gmail.com>
2022-05-05 21:52:18 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'iqa-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'interview-questions-app';
+}
